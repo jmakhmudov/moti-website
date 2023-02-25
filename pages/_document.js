7c1126174e1f544f469b7b7fc2766b045be4b0e6
Jaloliddin Makhmudov <makhmudov@tuta.io>
2023-02-25 14:22:04 +0500

    bckg

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import bg from "@/public/pattern.jpg"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{
+        backgroundImage: bg.src
+      }}>
         <Main />
         <NextScript />
       </body>
